298a58b55028a2901ea04817f72e2d9218c258a4
Sebastian Reichel <sre@ring0.de>
2012-07-27 02:16:37 +0200

    blame item with most attached barcodes

--- inventory/views.py
+++ inventory/views.py
@@ -78,4 +78,5 @@ def stats(request):
 		"number_of_items": models.Item.objects.count(),
 		"number_of_items_with_parent": models.Item.objects.filter(parent__isnull=False).count(),
 		"number_of_items_without_parent": models.Item.objects.filter(parent__isnull=True).count(),
+		"max_barcodes_item": models.Item.objects.raw('SELECT inventory_item.*, COUNT(inventory_barcode.item_id) AS number_of_attached_barcodes FROM inventory_item INNER JOIN inventory_barcode ON inventory_barcode.item_id = inventory_item.id GROUP BY inventory_barcode.item_id ORDER BY COUNT(inventory_barcode.item_id) DESC LIMIT 1;')[0],
 	})
