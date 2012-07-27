2829f53d788b4de178e01fe1f32729903244a6e8
Sebastian Reichel <sre@ring0.de>
2012-07-28 00:38:48 +0200

    add openid

--- inventar/urls.py
+++ inventar/urls.py
@@ -12,6 +12,8 @@ from inventory.views import stats
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
 
+    (r'^openid/', include('django_openid_auth.urls')),
+
     url(r'^item/(?P<selectedid>[0-9A-Za-z]{4})/$', item, name='item'),
     url(r'^search/(?P<term>[^/]+)/$', search, name='search'),
     url(r'^graph/$', graph, name='graph'),
