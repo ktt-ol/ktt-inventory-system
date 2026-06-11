bfed30f989a2038faac36cde09d8b3b879a8f783
Johannes Bornhold <johannes@bornhold.name>
2026-06-11 18:32:39 +0200

    test: Reproduce /stats/ crash on empty database

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,16 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class StatsViewTest(TestCase):
+    def test_stats_empty_database(self):
+        response = self.client.get("/stats/")
+        self.assertEqual(response.status_code, 200)
